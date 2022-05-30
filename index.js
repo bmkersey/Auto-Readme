7789c047f506b7a3e192454f7dca8575037f2efe
PickleRick <bmkersey@gmail.com>
2022-05-29 23:24:20 -0400

    created questions array

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const inquirer = require ('inquirer');
 const writeToFile = require ('./utils/generateMarkdown.js');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = ["What is the title of your project?", "Please enter a detailed description of your project:", "Please enter how to install:", "Enter an overview of usage:", "Enter how to contribute:", "Please enter test instructions:", ""];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
