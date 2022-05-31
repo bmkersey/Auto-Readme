0cbf780536f26a8f1f9a2ef08af318baffbeee41
PickleRick <bmkersey@gmail.com>
2022-05-31 01:40:44 -0400

    added license functionality, refactored main code

--- index.js
+++ index.js
@@ -4,120 +4,131 @@ const inquirer = require ('inquirer');
 const generateFile = require ('./utils/generateMarkdown.js');
 
 // TODO: Create an array of questions for user input
-const questions = ["What is the title of your project?", "Please enter a detailed description of your project:", "Please enter how to install:", "Enter an overview of usage:", "Enter how to contribute:", "Please enter test instructions:", ""];
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: questions[0],
+        validate: titleInput =>{
+            if (titleInput){
+                return true;
+            }else{
+                console.log('Please enter a title!')
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: questions[1],
+        validate: descriptionInput =>{
+            if (descriptionInput){
+                return true;
+            }else{
+                console.log('Please enter a description!')
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'install',
+        message: questions[2],
+        validate: installInput => {
+            if (installInput){
+                return true;
+            }else{
+                console.log('Please enter installation details!')
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: questions[3],
+        validate: usageInput => {
+            if (usageInput){
+                return true;
+            } else{
+                console.log('Please enter usage details!');
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'contribute',
+        message: questions[4],
+        validate: contributeInput => {
+            if (contributeInput){
+                return true;
+            } else{
+                console.log('Please enter contribution details!');
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'test',
+        message: questions[5],
+        validate: testInput => {
+            if (testInput){
+                return true;
+            } else{
+                console.log('Please enter test details!');
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'license',
+        message: questions[6],
+        validate: licenseInput => {
+            if (licenseInput){
+                return true;
+            } else{
+                return false;
+            }
+        }
+    },
+    {
+        type: 'list',
+        name: 'license',
+        message: 'Please select a license option for your project.',
+        choices: ['Apache 2.0 License', 'Boost Software License 1.0', 'BSD 3-Clause License', 'BSD 2-Clause License', 'GNU GPL v3', 'GNU GPL v2', 'GNU AGPL v3', 'GNU LGPL v3', 'The MIT License', 'The Unlicense'],
+        validate: licenseInput => {
+            if (!licenseInput) {
+                console.log('Please enter license information!');
+                return false
+            }
+            return true;
+        }
+    }
+];
+
 
 // TODO: Create a function to write README file
-const writeFile = fileContent =>{
-    return new Promise((resolve, reject)=>{
-        fs.writeFile('./dist/readme.txt', fileContent, err => {
-            // if there's an error, reject the Promise and send the error to the Promise's `.catch()` method
-            if (err) {
-                reject(err);
-              // return out of the function here to make sure the Promise doesn't accidentally execute the resolve() function as well
-                return;
-            }
-    
-            // if everything went well, resolve the Promise and send the successful data to the `.then()` method
-            resolve({
-                ok: true,
-                message: 'File created!'
-            });
-        });
-    });
-};
+function writeFile(fileName, data) {
+    fs.writeFile(fileName, data, err => {
+        if (err) {
+            return console.log(err);
+        }
+
+        console.log('Your readme has been generated!!')
+    })
+}
 
 // TODO: Create a function to initialize app
 const promptUser = () => {
-    return inquirer.prompt([
-        {
-            type: 'input',
-            name: 'title',
-            message: questions[0],
-            validate: titleInput =>{
-                if (titleInput){
-                    return true;
-                }else{
-                    console.log('Please enter a title!')
-                    return false;
-                }
-            }
-        },
-        {
-            type: 'input',
-            name: 'description',
-            message: questions[1],
-            validate: descriptionInput =>{
-                if (descriptionInput){
-                    return true;
-                }else{
-                    console.log('Please enter a description!')
-                    return false;
-                }
-            }
-        },
-        {
-            type: 'input',
-            name: 'install',
-            message: questions[2],
-            validate: installInput => {
-                if (installInput){
-                    return true;
-                }else{
-                    console.log('Please enter installation details!')
-                }
-            }
-        },
-        {
-            type: 'input',
-            name: 'usage',
-            message: questions[3],
-            validate: usageInput => {
-                if (usageInput){
-                    return true;
-                } else{
-                    return false;
-                }
-            }
-        },
-        {
-            type: 'input',
-            name: 'contribute',
-            message: questions[4],
-            validate: contributeInput => {
-                if (contributeInput){
-                    return true;
-                } else{
-                    return false;
-                }
-            }
-        },{
-            type: 'input',
-            name: 'test',
-            message: questions[5],
-            validate: testInput => {
-                if (testInput){
-                    return true;
-                } else{
-                    return false;
-                }
-            }
-        }
-    ]);
+    var answers = inquirer.prompt
+    .then(answers =>{
+        generateFile(answers)
+    })
+    .then(file =>{
+        writeFile('readme.md', file)
+    })
     
 };
 
 // Function call to initialize app
 promptUser()
-    .then(readmeData =>{
-        return generateFile(readmeData);
-    })
-    .then(fileContent => {
-        return writeFile(fileContent);
-    })
-    .then(writeFileResponse =>{
-        console.log(writeFileResponse);
-    })
-    .catch(err =>{
-        console.log(err);
-    })
-    
